dc12bb5952ed47e43151e26abc71642aeb393ccb
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2012-08-28 19:14:15 +0800

    added new Files

--- src/com/heralli/ekonsehan/Main.java
+++ src/com/heralli/ekonsehan/Main.java
@@ -1,17 +1,28 @@
 package com.heralli.ekonsehan;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JLabel;
+
 public class Main {
 	public static void main(String args[])
 	{
-		SplashScreen test = new SplashScreen();
-		/*
+		//SplashScreen test = new SplashScreen();
+		
 		MultipleChoiceQuestion q1 = new MultipleChoiceQuestion();
 		q1.setQuestion("Who is the father of Economics?");
 		q1.addChoice("a. Adam Smith"); 
 		q1.addChoice("b. Adam Sandler");
 		q1.addChoice("c. Alan Turing");
 		q1.addChoice("d. Alan K");
-		System.out.println(q1.getChoice(3));
-		*/
+		
+		MultipleChoiceQuestionView q1v = new MultipleChoiceQuestionView(q1);
+		MainWindow mainWin = new MainWindow();
+		mainWin.getContentPane().setLayout(new BorderLayout());
+		mainWin.getContentPane().add(new JLabel("Hello"),BorderLayout.WEST);
+		mainWin.getContentPane().add(q1v,BorderLayout.CENTER);
+		mainWin.setVisible(true);
+		
+		
 	}
 }
---
+++ src/com/heralli/ekonsehan/MainWindow.java
@@ -0,0 +1,20 @@
+package com.heralli.ekonsehan;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JWindow;
+
+public class MainWindow extends JFrame{
+		public MainWindow(){
+			//this.setExtendedState(MAXIMIZED_BOTH);
+			//this.setUndecorated(true);
+			this.setSize(100,200);
+		}	
+	    
+}
+
